f4d4a5a749f2ce303a7cb6341e0610d8092a6f95
kws1044 <kws1044@gmail.com>
2021-10-05 20:37:57 +0900

    <UsersMicroservice_2> UsersMicroservice - JWT 생성 처리 과정

--- userservice-01/src/main/java/com/example/userservice01/config/AuthenticationFilter.java
+++ userservice-01/src/main/java/com/example/userservice01/config/AuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.example.userservice01.config;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -21,6 +22,8 @@ import com.example.userservice01.service.UserService;
 import com.example.userservice01.vo.RequestLogin;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -62,6 +65,19 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter{
 		
 		String email = ((User)authResult.getPrincipal()).getUsername();
 		UserDto userDetails = userService.getUserDetailsByEmail(email);
+		
+		String token = Jwts.builder()
+				.setSubject(userDetails.getUserId())
+				.setExpiration(
+						new Date(
+								System.currentTimeMillis() + Long.parseLong(env.getProperty("token.expiration_time"))
+								)
+						)
+				.signWith(SignatureAlgorithm.HS512, env.getProperty("token.secret"))
+				.compact();
+		
+		response.addHeader("token", token);
+		response.addHeader("userId", userDetails.getUserId());
 	}
 
 	
