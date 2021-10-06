4e12503d03a5f0c22382a05f8a07f280ecf38589
kws1044 <kws1044@gmail.com>
2021-10-06 20:14:00 +0900

    <ConfigurationService> UsersMicroservice에서 Spring Cloud Config 연동

--- apigateway/src/main/java/com/example/apigateway/App.java
+++ apigateway/src/main/java/com/example/apigateway/App.java
@@ -1,7 +1,10 @@
 package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App {
@@ -10,4 +13,9 @@ public class App {
 		SpringApplication.run(App.class, args);
 	}
 
+	@Bean
+	public HttpTraceRepository httpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
+	}
+			
 }
